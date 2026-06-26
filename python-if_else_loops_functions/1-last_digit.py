ec941b9c309a41d3a9faa8c23cce8a0caebcd6f4
shyakanga <c.shyakanga@alustudent.com>
2026-06-26 04:20:59 -0700

    Add The last digit exercise

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+Snumber = str(number)
+ls_digit = Snumber[-1]
+if number < 0 and int(ls_digit) != 0:
+    ls_digit = "-"+ls_digit
+if int(ls_digit) > 5:
+    ls_comp = "and is greater than 5"
+elif int(ls_digit) == 0:
+    ls_comp = "and is 0"
+elif int(ls_digit) < 6 and int(ls_digit) != 0:
+    ls_comp = "and is less than 6 and not 0"
+    
+print(f"Last digit of {number} is {ls_digit} {ls_comp}")
