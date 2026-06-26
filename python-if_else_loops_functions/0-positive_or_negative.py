2640f6ad4f72e1b2c3a8a11b3479f4c59d91bf32
shyakanga <c.shyakanga@alustudent.com>
2026-06-26 03:47:33 -0700

    Add Positive anything is better than negative nothing PEP8 correction exercise

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-print(number, "is positive" if number > 0 else "is negative" if number < 0 else "is zero")
+result = (
+    "is positive" if number > 0 else
+    "is negative" if number < 0 else
+    "is zero"
+)
+print(f"{number} {result}")
