463dc23e6e8197dc93d3e61fecdddcb789cf8406
shyakanga <c.shyakanga@alustudent.com>
2026-07-07 07:33:21 -0700

    Add  exercise

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_int = 0 if len(my_list) > 1 else None
+    max_int = 0 if len(my_list) >= 1 else None
     for x in my_list:
         if x > max_int:
             max_int = x
