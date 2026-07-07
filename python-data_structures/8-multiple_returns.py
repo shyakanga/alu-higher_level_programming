b167c60b0766491b8195d24865d13d202eb3602b
shyakanga <c.shyakanga@alustudent.com>
2026-07-07 07:16:25 -0700

    Add More returns! exercise

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    f_char = [x for x in sentence]
+    f_char = sentence[0] if length > 0 else None
     return (length, f_char[0])
