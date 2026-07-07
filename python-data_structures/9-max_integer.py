4dd9b9fbc1cc67b3fcf9c7f0279ee174fab3da03
shyakanga <c.shyakanga@alustudent.com>
2026-07-07 07:32:09 -0700

    Add Find the max correction exercise

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    return max(my_list) if len(my_list) > 1 else None
+    max_int = 0 if len(my_list) > 1 else None
+    for x in my_list:
+        if x > max_int:
+            max_int = x
+    return max_int
