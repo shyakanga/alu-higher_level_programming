07b42cb892ec0e0fed339451a3f25b5b270aaf17
shyakanga <c.shyakanga@alustudent.com>
2026-07-10 07:25:06 -0700

    Add exercise "Roman to Integer"

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_chr = {"I": 1, "V": 5, "X": 10, "L": 50,"C": 100, "D": 500,"M": 1000}
+    chara = [roman_chr[l] for l in roman_string]
+    return sum(chara)
