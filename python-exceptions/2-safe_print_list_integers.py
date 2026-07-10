dbe4dfaa2b345f9ea730408b611ffe3b777e9ec6
shyakanga <c.shyakanga@alustudent.com>
2026-07-10 07:46:39 -0700

    Add exercise "Print and count integers correction"

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,12 +3,11 @@ def safe_print_list_integers(my_list=[], x=0):
     nb_print = 0
     try:
         for i in range(x):
-            if isinstance(my_list[i], int) and not isinstance(my_list[i], bool):
-                print("{:d}".format(my_list[i]), end="")
+            val = my_list[i]
+            if isinstance(val, int) and not isinstance(val, bool):
+                print("{:d}".format(val), end="")
                 nb_print += 1
-    except (ValueError, TypeError):
-        pass
-    except IndexError:
+    except ValueError:
         pass
     print()
     return nb_print
