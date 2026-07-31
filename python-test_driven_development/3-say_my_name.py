265fd0fe90f2e6f5ffa514a5126cea7fac36243a
shyakanga <c.shyakanga@alustudent.com>
2026-07-31 11:26:55 -0700

    Add exercise "correction of exercise 3"

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """
-This module contains a function that prints a formatted name.
+This module contains the function say_my_name.
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    Prints My name is <first name> <last name>
+    """Prints 'My name is <first name> <last name>'.
 
     Args:
-        first_name: string representing first name
-        last_name: string representing last name (default "")
+        first_name: The first name string.
+        last_name: The last name string (default is "").
 
     Raises:
-        TypeError: if first_name or last_name are not strings
+        TypeError: If either first_name or last_name is not a string.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+
     print("My name is {} {}".format(first_name, last_name))
