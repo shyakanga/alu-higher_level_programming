d95c5e0c24a704758c2c413ca75b92b4c012fe73
shyakanga <c.shyakanga@alustudent.com>
2026-07-31 10:22:57 -0700

    Add exercise "correction for exercise 4"

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with header X-HolbertonSchool-User-Id set to 98
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to the URL passed as argument with a custom header
+curl -s -H "X-School-User-Id: 98" "$1"
