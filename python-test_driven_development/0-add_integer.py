9eab511d8314f2619c96c6afa16a0d53c6394efd
shyakanga <c.shyakanga@alustudent.com>
2026-07-31 11:32:44 -0700

    Add exercise "correctio nexecise 1"

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -17,16 +17,15 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If a or b is not an integer/float, or is NaN/infinity.
     """
-    if not isinstance(a, (int, float)):
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-
     try:
         a = int(a)
     except (OverflowError, ValueError):
         raise TypeError("a must be an integer")
 
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
     try:
         b = int(b)
     except (OverflowError, ValueError):
