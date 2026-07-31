aed331e31c350fb127e477b2e0e793040c03c15a
shyakanga <c.shyakanga@alustudent.com>
2026-07-31 10:46:40 -0700

    Add exercise "correction for exercise 4"

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status using requests package."""
+"""
+Script that fetches https://intranet.hbtn.io/status using requests
+"""
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://alu-intranet.hbtn.io/status")
+    r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
