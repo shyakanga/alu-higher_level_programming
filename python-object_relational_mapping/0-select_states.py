23fdaebfc1c9697d0fd8b12631e030861623959b
Shyaka Carrick <c.shyakanga@alustudent.com>
2026-07-31 23:42:57 +0200

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     db = MySQLdb.connect(
-    host="localhost", port=3306, user=username,
-    passwd=password, db=db_name, charset="utf8")
+        host="localhost", port=3306, user=username,
+        passwd=password, db=db_name, charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
